92abf8bd6f7a6d64fc48fc15ce3ddec33ec22440
Mohamed <sifosman@gmail.com>
2025-04-02 16:48:48 +0200

    Fix shop page filters by updating ProductFilters component to support new controlled35

--- components/PriceRangeFilter.tsx
+++ components/PriceRangeFilter.tsx
@@ -21,14 +21,15 @@ const PriceRangeFilter = memo(({
       <div className="flex items-center justify-between">
         <h3 className="text-md font-lato font-bold text-black">Price Range</h3>
       </div>
-      <div className="flex items-center gap-2">
-        <Slider.Root
+      <Slider.Root
           className="relative flex items-center select-none touch-none w-full h-5"
-          value={[currentMin]}
+          value={[currentMin, currentMax]}
           min={minPrice}
-          max={currentMax}
+          max={maxPrice}
           step={1}
-          onValueChange={(newValues) => onPriceChange(newValues[0], currentMax)}
+          onValueChange={(newValues) => {
+            onPriceChange(newValues[0], newValues[1]);
+          }}
         >
           <Slider.Track className="bg-gray-600 relative grow rounded-full h-[3px]">
             <Slider.Range className="absolute bg-green-700 rounded-full h-full" />
@@ -37,24 +38,11 @@ const PriceRangeFilter = memo(({
             className="block font-lato text-black w-5 h-5 bg-white shadow-md rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-green-700"
             aria-label="Min price"
           />
-        </Slider.Root>
-        <Slider.Root
-          className="relative flex items-center select-none touch-none w-full h-5"
-          value={[currentMax]}
-          min={currentMin}
-          max={maxPrice}
-          step={1}
-          onValueChange={(newValues) => onPriceChange(currentMin, newValues[0])}
-        >
-          <Slider.Track className="bg-gray-600 relative grow rounded-full h-[3px]">
-            <Slider.Range className="absolute bg-green-700 rounded-full h-full" />
-          </Slider.Track>
           <Slider.Thumb
             className="block font-lato text-black w-5 h-5 bg-white shadow-md rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-green-700"
             aria-label="Max price"
           />
         </Slider.Root>
-      </div>
       <div className="flex justify-between mt-1">
         <span className="text-sm font-lato text-black">
           R{Math.round(currentMin).toLocaleString()}
