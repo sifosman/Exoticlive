121a4f0b99b399e13b7070c8afc6a0b2a903c4e2
Mohamed <sifosman@gmail.com>
2025-04-04 17:46:11 +0200

    Fix product page to only use ProductContentSimplified component222

---
+++ lib/constants.ts
@@ -0,0 +1,22 @@
+// Site information
+export const SITE_NAME = 'Exotic Shoes';
+export const SITE_DESCRIPTION = 'Exotic Shoes - South Africa\'s Premier Shoe Store';
+export const SITE_URL = 'https://exoticshoes.co.za';
+
+// Stock status constants
+export const STOCK_STATUS_IN_STOCK = 'instock';
+export const STOCK_STATUS_OUT_OF_STOCK = 'outofstock';
+export const STOCK_STATUS_ON_BACKORDER = 'onbackorder';
+
+// API endpoints
+export const API_ENDPOINTS = {
+  PRODUCT: '/api/product',
+  VARIATION: '/api/variation',
+  STOCK_UPDATE: '/api/stock/update',
+  STOCK_LISTENER: '/api/stock-listener',
+  REVALIDATE: '/api/revalidate'
+};
+
+// Default values
+export const DEFAULT_PAGE_SIZE = 12;
+export const DEFAULT_MAX_QUANTITY = 99;
